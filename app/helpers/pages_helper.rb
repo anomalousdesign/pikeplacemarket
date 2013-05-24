d9757f4f1fea140e542bd619dc817a5f34e8e495
Gabe Coyne <gabe@killitcreative.com>
2013-05-24 10:13:46 -0700

    fix dropdowns

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -12,15 +12,15 @@ module PagesHelper
   	args[:pages] ||= Page.live.where("ancestry is ?", nil)
     args[:pages] = args[:pages].order("position")
     html = ""
-  	html << "<ul class='#{ args[:class] || "" }'>"
+  	html << "<ul class='#{ args[:class] || "" }' role='menu'>"
   	if args[:include_home]
   		args [:home_label] ||= "Home"
 	  	html << "<li class='home'><a href='/'>#{args[:home_label]}</a></li>"
 	  end
     args[:pages].each do |page|
-      html << "<li class='#{page.label.gsub(" ","_").downcase}'>"
+      html << "<li class='#{page.label.gsub(" ","_").downcase} #{!page.children.empty? ? "dropdown-submenu" : ""}'>"
       html << link_to(page.label, page_path(page))
-      html << page_nav(pages: page.children) if !page.children.empty?
+      html << page_nav(pages: page.children, class: "dropdown-menu") if !page.children.empty?
       html << "</li>"
     end
     html << "</ul>"

ca198b273eba1c47437f8a3b3c9a60475b384ba7
Gabe Coyne <gabe@killitcreative.com>
2013-05-28 22:30:57 -0700

    updates

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -18,7 +18,7 @@ module PagesHelper
 	  	html << "<li class='home'><a href='/'>#{args[:home_label]}</a></li>"
 	  end
     args[:pages].each do |page|
-      html << "<li class='#{page.label.gsub(" ","_").downcase} #{!page.children.empty? ? "dropdown-submenu" : ""}'>"
+      html << "<li class='#{page.label.gsub(" ","_").downcase} #{!page.children.empty? && args[:menu].nil? ? "dropdown-submenu" : ""}'>"
       html << link_to(page.label, page_path(page))
       html << page_nav(pages: page.children, class: "dropdown-menu") if !page.children.empty?
       html << "</li>"

a85c399bc4c69536bdac2ae77d2c60340a064971
Gabe Coyne <gabe@killitcreative.com>
2013-06-06 08:37:11 -0700

    switch promos to s3

--- app/models/promo.rb
+++ app/models/promo.rb
@@ -1,4 +1,14 @@
 class Promo < ActiveRecord::Base
   # attr_accessible :title, :body
-  has_attached_file :image, :styles => { :large => "710x445#", :thumb => "200x200#" }
+  config = YAML.load(File.read('config/s3.yml'))[Rails.env]
+  has_attached_file :image, 
+    :styles => { 
+      :large =>        "710x445#",
+      :thumb =>        "200X200#"
+    },
+    :storage => :s3,
+    :s3_credentials => "#{Rails.root}/config/s3.yml",
+    :s3_host_alias => config["s3_host_alias"],
+    :url => ":s3_alias_url",
+    :path => "/paperclip/:class/:id/:style/:filename"
 end

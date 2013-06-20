a8e07db43faaf0978fc23552c78d9d1fbfbb0db1
Gabe Coyne <gabe@killitcreative.com>
2013-06-19 23:47:17 -0700

    add image to review email

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,8 +11,11 @@ class EventsController < ApplicationController
 		flash[:notice] = "Thanks! We will review your event."
 		msg = [
 			AppMailer.hash_table("New Event", event.attributes),
-			"<br /><br />Review Now?<br />",
-			"http://#{request.host}/admin/event/#{event.id}/edit"
+			"<br /><br /><h3>Review Now?</h3>",
+			"http://#{request.host}/admin/event/#{event.id}/edit",
+			"<br /><br />",
+			"<img width='400' src='#{Editables::EditableImage.find_by_name("Event #{event.id} Header").image.url}' />",
+			"<br /><br />",
 		].join("")
 		AppMailer.send_mail("gabecoyne@gmail.com", "Event Needs Review", msg).deliver
 		redirect_to :back

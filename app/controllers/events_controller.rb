f89079bf17b48e2f954a2462d90de6d19e6b5af6
Gabe Coyne <gabe@killitcreative.com>
2013-06-19 23:34:25 -0700

    slider caption color

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,7 +11,7 @@ class EventsController < ApplicationController
 		flash[:notice] = "Thanks! We will review your event."
 		msg = [
 			AppMailer.hash_table("New Event", event.attributes),
-			"<br /><br />Review<br />",
+			"<br /><br />Review Now?<br />",
 			"http://#{request.host}/admin/event/#{event.id}/edit"
 		].join("")
 		AppMailer.send_mail("gabecoyne@gmail.com", "Event Needs Review", msg).deliver

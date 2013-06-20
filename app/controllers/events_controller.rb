f44e8207e489a570efe5e25af722245709ee7bf0
Gabe Coyne <gabe@killitcreative.com>
2013-06-20 09:47:10 -0700

    fix caption color and event submit

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,10 +13,15 @@ class EventsController < ApplicationController
 			AppMailer.hash_table("New Event", event.attributes),
 			"<br /><br /><h3>Review Now?</h3>",
 			"http://#{request.host}/admin/event/#{event.id}/edit",
-			"<br /><br />",
-			"<img width='400' src='#{Editables::EditableImage.find_by_name("Event #{event.id} Header").image.url}' />",
-			"<br /><br />",
+			"<br /><br />"
 		].join("")
+		img = Editables::EditableImage.find_by_name("Event #{event.id} Header")
+		if img.present?
+			msg += [			
+				"<img width='400' src='#{img.image.url}' />",
+				"<br /><br />",
+			]
+		end
 		AppMailer.send_mail("gabecoyne@gmail.com", "Event Needs Review", msg).deliver
 		redirect_to :back
 	end

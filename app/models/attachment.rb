09eb3e7404a645ce3033274b92c2c5c7c04da681
Gabe Coyne <gabe@killitcreative.com>
2013-07-10 09:45:05 -0700

    update attachments

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,3 +1,9 @@
 class Attachment < ActiveRecord::Base
-	has_attached_file :file
+	config = YAML.load(File.read('config/s3.yml'))[Rails.env]
+  has_attached_file :file,
+    :storage => :s3,
+    :s3_credentials => "#{Rails.root}/config/s3.yml",
+    :s3_host_alias => config["s3_host_alias"],
+    :url => ":s3_alias_url",
+    :path => "/paperclip/:class/:id/:style/:filename"
 end

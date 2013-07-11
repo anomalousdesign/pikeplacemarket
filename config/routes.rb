a1bbae6aa907e832b40d0f34be16b907688e9b83
Gabe Coyne <gabe@killitcreative.com>
2013-07-10 18:32:45 -0700

    add redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 PikeplacemarketOrg::Application.routes.draw do
 
+	get '/news_events/pc1north', to: redirect('/pages/pc-1-north-rfp-rfq')
+
   devise_for :admins
 
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'

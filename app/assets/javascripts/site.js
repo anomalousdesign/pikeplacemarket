0c9191fafcc48f35ff772e56fdfed887376837a6
Gabe Coyne <gabe@killitcreative.com>
2013-07-22 11:26:32 -0700

    add google analytics

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -155,3 +155,15 @@ $(function(){
 if(navigator.userAgent.match("MSIE")){
 	$("head").append('<link href="/ie.css" media="all" rel="stylesheet" type="text/css" />')
 }
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-16887145-1']);
+_gaq.push(['_setDomainName', 'pikeplacemarket.org']);
+_gaq.push(['_setAllowLinker', true]);
+_gaq.push(['_trackPageview']);
+
+(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();

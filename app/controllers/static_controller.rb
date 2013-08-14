c90da903cf5c24ed9b704e1ce96a9d7d444ca3c5
Gabe Coyne <gabe@killitcreative.com>
2013-08-14 08:56:56 -0700

    add honeypot

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,6 +3,14 @@ class StaticController < ApplicationController
   caches_action :show, :unless => Proc.new{|x| x.current_admin.present? }, :expires_in => 1.hour
 
   def signup
+  	if params[:company].present?
+  		flash[:alert] = "We think you are a robot... Please contact if info@pikeplacemarket.org if you are a human."
+  		return redirect_to :back
+  	end
+  	if params[:email].empty?
+  		flash[:alert] = "We need at least need your email to sign you up."
+  		return redirect_to :back
+  	end
   	Subscriber.create name: "#{params[:first_name]} #{params[:last_name]}", email: params[:email]
   	flash[:notice] = "Thanks for subscribing!"
   	redirect_to :back

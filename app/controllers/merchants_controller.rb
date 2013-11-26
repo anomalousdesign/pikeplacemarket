c9ccb40b5716d8cc214858c82d8a033ce226b9f3
Gabe Coyne <gabe@killitcreative.com>
2013-11-25 23:18:16 -0800

    add merchants json feed

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,3 +1,26 @@
 class MerchantsController < ApplicationController
 	caches_action :index, :show, :unless => Proc.new{|x| x.current_admin.present? }, :expires_in => 1.day
+
+	def index
+		respond_to do |format|
+			format.json{
+				@merchants = {}
+				MerchantCategory.main.each{|category|
+					@merchants[category.name] = {}
+					category.children.map{|child|
+						@merchants[category.name][child.name] = child.merchants.as_json
+					}
+				}
+				render json: @merchants
+			}
+			format.html{
+				render :index
+			}
+		end
+	end
+
+	def category_json(category)
+		category.children.as_json
+	end
+
 end

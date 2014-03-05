42d46e76d96d9d03cd3d3ac78954d77d422b576b
Noski <stephen@sweckercreative.com>
2014-03-04 17:01:09 -0800

    Upgrading Newsletter Signup Security

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,19 +1,23 @@
 class StaticController < ApplicationController
 	
-  caches_action :show, :unless => Proc.new{|x| x.current_admin.present? }, :expires_in => 1.hour
-
-  def signup
-  	if params[:company].present?
-  		flash[:alert] = "We think you are a robot... Please contact if info@pikeplacemarket.org if you are a human."
-  		return redirect_to :back
-  	end
-  	if params[:email].present? && params[:email].empty?
-  		flash[:alert] = "We need at least need your email to sign you up."
-  		return redirect_to :back
-  	end
-  	Subscriber.create name: "#{params[:first_name]} #{params[:last_name]}", email: params[:email]
-  	flash[:notice] = "Thanks for subscribing!"
-  	redirect_to :back
-  end
-
+    caches_action :show, :unless => Proc.new{|x| x.current_admin.present? }, :expires_in => 1.hour
+    
+    def signup
+        if params[:category].empty? || params[:category] != DateTime.current.utc.strftime("%Y%m%d")
+            flash[:alert] = "Thanks for subscribing!"
+            return redirect_to :back
+        end
+        if params[:website].present?
+            flash[:alert] = "Thanks for subscribing!"
+            return redirect_to :back
+        end
+        if params[:email].present? && params[:email].empty?
+            flash[:alert] = "We need at least need your email to sign you up."
+            return redirect_to :back
+        end
+        Subscriber.create name: "#{params[:first_name]} #{params[:last_name]}", email: params[:email]
+        flash[:notice] = "Thanks for subscribing!"
+        redirect_to :back
+    end
+    
 end

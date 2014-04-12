05b8e8df499b9279788c6370aa8f887481932fac
Noski <stephen@sweckercreative.com>
2014-04-11 22:19:12 -0700

    Revert 42d46e7..58e5d55
    This rolls back to commit 42d46e76d96d9d03cd3d3ac78954d77d422b576b.

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,12 +1,5 @@
 class MerchantsController < ApplicationController
 	caches_action :index, :show, :unless => Proc.new{|x| x.current_admin.present? }, :expires_in => 1.day
-	before_filter :check_host
-
-	def check_host
-		if request.host.split('.')[0] == 'www'
-			redirect_to "http://" + request.host.gsub('www.','')
-		end
-	end
 
 	def index
 		respond_to do |format|

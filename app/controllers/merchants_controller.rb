58e5d555228e1e18f7c4e693be6330e1722d1960
Noski <stephen@sweckercreative.com>
2014-04-11 22:13:41 -0700

    Attempting temporary /directory workaround.

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,5 +1,12 @@
 class MerchantsController < ApplicationController
 	caches_action :index, :show, :unless => Proc.new{|x| x.current_admin.present? }, :expires_in => 1.day
+	before_filter :check_host
+
+	def check_host
+		if request.host.split('.')[0] == 'www'
+			redirect_to "http://" + request.host.gsub('www.','')
+		end
+	end
 
 	def index
 		respond_to do |format|
